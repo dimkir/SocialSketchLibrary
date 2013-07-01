bcd2051ea9222a867bed96b03aaf28536235c58c
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-07-01 13:09:52 +0100

    Added text() method
    This text() method should be the only way text is rendered from the
    library.

--- libraryexample/FontBoss.java
+++ libraryexample/FontBoss.java
@@ -1,5 +1,9 @@
 /**
  *  Just fancy name class which encapsulates operations with fonts.
+ * 
+ * ? how do we distribute fonts with the library? 
+ * or actually how do we access actual font with the library? 
+ * should I incorportate it as a resource inside of the JAR?
  */
 class FontBoss extends AbstractLibraryHelper
 {
@@ -21,5 +25,25 @@ class FontBoss extends AbstractLibraryHelper
   private void initFont(){
      mFont = parent.createFont(C_DEFAULT_FONT_NAME, C_DEFAULT_FONT_SIZE); 
   }  
+  
+  
+  
+  
+  /**
+   * this is convenience method to output the text (and be sure that we have size and stuff set
+   * TODO: this interferes with the state of the sketch (changes it's font etc and 
+   *  it DOESN'T change font color/outline. Thus wa may not render it correctly.
+   */
+  void text(String s, float xx , float yy){
+       // there's a problem,
+       // we can't really save the parameters of the fonts.
+       // thus we interfere with the sketch state and results may
+       // not be as we want :)
+       sketch().textFont(mFont);
+       sketch().textSize(C_DEFAULT_FONT_SIZE);
+       //?? and what about color?
+       sketch().text(s, xx ,yy);
+  }
+  
     
 }
