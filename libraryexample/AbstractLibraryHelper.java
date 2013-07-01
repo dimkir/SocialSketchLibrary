67eea93c1e03bfaa163c9d00dadb85ce6760389e
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-07-01 13:13:43 +0100

    Added some core functionality
    Added getters for the parent (two synonymous) and also added log()
    method to log to sketch console and as well added verification for the
    null parameter for the parent. So that we can crash ASAP in case null is
    parsed.

--- libraryexample/AbstractLibraryHelper.java
+++ libraryexample/AbstractLibraryHelper.java
@@ -4,10 +4,34 @@
  */
 abstract class AbstractLibraryHelper extends AbstractLibraryBase // incuim the base only saves .parent and defines println()
 {
+  private PApplet mParent; 
+   
+  
+  AbstractLibraryHelper(PApplet parent){
+    if ( parent == null ){
+       throw new IllegalParameterException("When initializing library Parent cannot be null");
+    }
+    
+     mParent = parent;
+  }
+  
+  protected PApplet sketch(){
+     return mParent;
+  }
+  
+  protected PApplet parent(){
+     return mParent;
+  }
   
    @Override
    public void toString(){
+      // todo: if we don't override, then the client may be calling toString and may be failing.. haha
       throw new RuntimeException("The abstract library helper: toString() should be OBLIGATORY overridden by the subclass");
+   }
+   
+   
+   protected void log(String s){
+      mParent.println(s);
    }
   
 }
