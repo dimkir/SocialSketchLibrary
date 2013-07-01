b60c0ed5042dbab60a972f38e4ce96d67c20bb15
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-07-01 16:43:09 +0100

    Fixed class to compile. Added reference to LibraryDrawing
    now drawing will be performed through LibraryDrawing.

--- libraryexample/Menu.java
+++ libraryexample/Menu.java
@@ -1,21 +1,32 @@
+package libraryexample;
+import processing.core.*;
 /**
- *  This class is to abstract the display of the menu.
+ *  This class is to abstract displaying of the on screen menu.
  */
 class Menu extends AbstractLibraryHelper
 {
+  
+  private LibraryDrawing mLD;
+  
+  
+  Menu(PApplet parent, LibraryDrawing ld){
+     super(parent);
+     mLD = ld;
+  }
   
   
   /**
    * Returns flag.
    */
   boolean isDisplayedFlag(){
-    
+    //TODO: what does this do?
+    return false;
   }
   
   /**
    * Draws to default rednerer, stated in the beginning.
    */
   void draw(){
-     ?? draw menu?
+    // TODO: implement some drawing of the menu.
   }
 }
