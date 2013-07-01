03738509ff7c63439141ff18af8edfa191cced2a
Ftp User i3 Wingit <ftp_user@inbox.lv>
2013-07-01 17:10:49 +0100

    Significant changes see details
    Added data structure:
    1) to hold logon credendials (map mLogOnCredentials Map<String,
    String>()
    2) to hold updated log strings FixedStringLog. (this is going to hold
    info of what's happening to twitter). And size constant.
    
    Also fixed constructor of TweetDirector, so that it throws exception in
    case null credentials supplied.
    
    Made a stub for tweetTheMessage() method, now we kinda can run this
    class to test. TODO: actual tweeting functionality.
    
    Added shutdown() method contents.
    
    Fixed getLogObject() method thought which we can figure out what's
    happening with the thing.
    Should this object be actually synchronized??? (it is going to be
    between TweetDirector and TweetThead)
    
    Added getLogObject() method (so that responsible for the display of the
    tweet object can access status).

--- libraryexample/TweetDirector.java
+++ libraryexample/TweetDirector.java
@@ -1,5 +1,8 @@
+package libraryexample;
 // import some twitter library here.
 
+import java.util.Map;
+import processing.core.*;
 /**
  * This class encapsulates and fluentizes operations with twitter.
  * It uses background thread, this is why it should be initialized and then
@@ -11,6 +14,14 @@
 class TweetDirector extends AbstractLibraryHelper
   implements IConfigXmlSpecification
 {
+  
+   private Map<String, String> mLogOnCredentials;
+   
+   /**
+    * This is log object, holding last status messages
+    */
+   private FixedStringLog mDirectorLog;
+   private final static int C_LOG_SIZE = 10;
   
    /**
     * @var mTweetThread thread where all the stuff takes b
@@ -24,10 +35,11 @@ class TweetDirector extends AbstractLibraryHelper
     * @param logOnCredentials is Map which is providing twitter log on credentials. 
     * NOT NULL. 
     * 
-    * @throws ??? some kind of exception saying that tweet director couldn't initialize.
+    * @throws TweetDirectorEx some kind of exception saying that tweet director couldn't initialize.
     * 
     */
-   TweetDirector(PApplet parent, Map<String,String> logOnCredentials){
+   TweetDirector(PApplet parent, Map<String,String> logOnCredentials) throws TweetDirectorEx
+   {
       super(parent);
       // make quick verification of the credentials. 
       // at least they shoudln't be null, because I don't want to 
@@ -36,28 +48,27 @@ class TweetDirector extends AbstractLibraryHelper
       if ( logOnCredentials == null){
          throw new TweetDirectorEx("Log on credentials cannot be null");
       }
+      
+      mLogOnCredentials = logOnCredentials;
+      mDirectorLog = new FixedStringLog(C_LOG_SIZE);
    }
    
-   
-   /**
-    * @param listener listener. Or null in case you want to reset listener.
-    */
-   void setOnAuthenticationFinishedListener(IOnAuthenticationFinished listener){
-     
-      ??? 
-   }
-   
-   
+  
    /**
     *  Tweets the message (actually submits to thread to tweet)
     */
-   void tweetTheMessage(String tweetMsg, ??? Image img){
-        mTweetThread.submitMessage(tweetMsg, Image img);
+   void tweetTheMessage(String tweetMsg, PImage img){
+        mTweetThread.submitMessage(tweetMsg, img);
         log("Submitted message [" + tweetMsg + "] and PImage");
         
    }
-      
-      
+   
+   /**
+    * Logs to our fixed string log object
+    */
+   void log(String s){
+       mDirectorLog.put(s);
+   }
       
     /**
      *  Starts the thread which is going to send tweets asynchronousely.
@@ -69,23 +80,31 @@ class TweetDirector extends AbstractLibraryHelper
     }
       
     /**
-     * Is called when sketch is to shut down. Basically stops the upload thread.
+     * Is called by main library class to shutdown worker thread.
      */
     void shutdown()
     {
-       ?? shutdown background thread.. but what do we do in case the tweeting process is still on?
+       // ?? shutdown background thread.. but what do we do in case the tweeting process is still on?
+      if  ( mTweetThread != null ){
+         mTweetThread.setRunning(false); // thread will shutdown eventually.
+      }
+      else{
+         println("TweetDirector::shutdown() :: TweetThread is null");
+      }
     }
     
     
     /**
-     * Returns stringar with last status messages (the i0 is the freshiest log message and the iN is the oldest message)
+     * Returns FixedStringLog with last status messages.
+     * (the i0 is the freshiest log message and the iN is the oldest message)
+     * 
      * @returns  ?? can we return NULL?? what do we do in case there's no log messages?
      *           is it actually a good structure to return? because it appears that we only need the few latest items. This can be
      *           implemented with String[], but we also need two more variables "qty and start". this way we will be able to contain log messages in fixed size-array.
      */
-    String[] getLogStrings(){
+    FixedStringLog getLogObject(){
       // should there always be at least one message in the log??
-       ???
+      return mDirectorLog;
     }
     
     
@@ -102,7 +121,16 @@ class TweetDirector extends AbstractLibraryHelper
       // here go the method to 
       // do the things:
       // ???
+      Map<String, String> getCredentials(){
+         return mLogOnCredentials;
+      }
       
+      /**
+       * Just way (for TwitterThread to communicate with log console)
+       */
+      void println(String s){
+         TweetDirector.this.println(s);
+      }
     }
     
 }
