8127af3f3fc97623d6330f9f3ac4efcee1e32136
Ftp User <ftp_user@inbox.lv>
2013-07-03 15:56:53 +0100

    For test-running puposes added functinoality for the menu to display thumbnail.

--- src/org/twitshot/Menu.java
+++ src/org/twitshot/Menu.java
@@ -8,11 +8,13 @@ class Menu extends AbstractLibraryHelper
   
   private LibraryDrawing mLD;
   private boolean mIsDisplayed;
-
+  private PImage tempImage;
+  private FontBoss mFB;
   
-  Menu(PApplet parent, LibraryDrawing ld){
+  Menu(PApplet parent, LibraryDrawing ld, FontBoss fb){
      super(parent);
      mLD = ld;
+     mFB = fb; // font boss
   }
   
   
@@ -35,5 +37,23 @@ class Menu extends AbstractLibraryHelper
    */
   void draw(){
     // TODO: implement some drawing of the menu.
+      // if temp image is available: then 
+      // do things.
+      if ( tempImage != null ){
+          // draw the image
+          mLD.image(tempImage, 0, 0);
+      }
+      
+      mFB.text("MENU TEXT", sketch().width / 2, sketch().height /2 );
   }
+
+  /**
+   * Just sets image to display. The image should not be a "live" graphics, 
+   * but some safely stored static image. This class is going to save reference
+   * to the image (no defensive copying here).
+   * @param screenShotOriginalSize  is the client actually live PGraphics???
+   */
+    void setImageToDisplay(PImage screenShotOriginalSize) {
+           tempImage = screenShotOriginalSize;
+    }
 }
