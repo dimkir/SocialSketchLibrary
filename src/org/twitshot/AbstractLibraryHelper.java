fb27362013c87222054d3518f3537bcd947abaef
Ftp User <ftp_user@inbox.lv>
2013-07-03 15:48:49 +0100

    Added method to test reading a resource file from jar

--- src/org/twitshot/AbstractLibraryHelper.java
+++ src/org/twitshot/AbstractLibraryHelper.java
@@ -1,4 +1,8 @@
 package org.twitshot;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import processing.core.*;
 /**
  * This is just abstract superclass which all library-helpers are inheriting.
@@ -23,5 +27,33 @@ abstract class AbstractLibraryHelper extends AbstractLibraryBase // incuim the b
      return mParent;
   }
   
+  
+  /**
+   * This is test method, we try here reading a resource 
+   * text file and output it to console. 
+   * @param resourcePath 
+   */
+  protected void testReadingFileFromResources(String resourcePath){
+      try {
+          InputStream is = getClass().getResourceAsStream(resourcePath);
+          InputStreamReader isr = new InputStreamReader(is);
+          BufferedReader br = new BufferedReader(isr);
+          
+          String line;
+          while ( ( line = br.readLine()) != null ){
+              println(line);
+          }
+          
+          br.close();
+          is.close();
+          
+      } catch (IOException ex) {
+          println("AbstractLibraryHelper::testReadingFileFromResources: " + ex.getMessage());
+      }
+      
+      
+      
+      
+  }
  
 }
