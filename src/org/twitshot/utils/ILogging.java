30abd31ad74148a329370750bd3e6e963e97f027
Dimitry K Lenovo <dimkir@gmail.com>
2013-07-05 17:36:43 +0100

    added ILogging interface and simpliest implementation ConsoleLogger

---
+++ src/org/twitshot/utils/ILogging.java
@@ -0,0 +1,10 @@
+package org.twitshot.utils;
+
+/**
+ * This is interface which can be passed to
+ * classes which are used to writing messages to console.
+ * @author dimki_000
+ */
+public interface ILogging {
+    void println(String msg);
+}
