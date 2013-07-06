956e8eb45ae7943376354d51fd53fa8dfe3a3674
Dimitry K Lenovo <dimkir@gmail.com>
2013-07-06 11:02:19 +0100

    Added few methods for making testing easier

--- src/org/sketchshot/utils/ExponentialBackoff.java
+++ src/org/sketchshot/utils/ExponentialBackoff.java
@@ -28,11 +28,16 @@ public class ExponentialBackoff {
     private boolean mLastStatusSuccess = true; 
     private long mLastAttemptTime = -1;
     
-    private long mCurrentWaitTimeMillis = 0;
+    /**
+     * This is amount of time which we need to wait
+     * before the next retry is allowed. This is the value 
+     * which will be exponentially increasing.
+     */
+    private long mCurrentDelayMillis = 0;
     private float mWaitTimeFactor = 1.0f; // 
     private long mMaxDelayMillis = -1;
-    
-    private final static long C_MIN_WAIT_TIME_MILLIS = 3000; // 3 seconds minimum time.
+    public final static int C_MIN_WAIT_TIME_SECONDS = 3;
+    private final static long C_MIN_WAIT_TIME_MILLIS = C_MIN_WAIT_TIME_SECONDS * 1000; // 3 seconds minimum time.
     
     boolean mVerbose = false;
     
@@ -40,14 +45,20 @@ public class ExponentialBackoff {
     /**
      * Initializes EB object.
      * @param multiplier    by which factor we should multiply the time every time?
+     *                      number should be 1.0 and above? (can't we just make it smaller?
+     *                      (it doesn't make any sense).
      * @param maxDelaySeconds  we can limit max delay by this time.
+     *                       shouldn't be less than #C_MIN_WAIT_TIME_SECONDS
      * @param verboseFlag   set to true if you want to get extra debug messages to console.
      *                      // the question is: how to send messages to console if this
      *                      // class doesn't know what a console is? or can be run on a
      *                      // device w/o console?
      *                      // TODO: add some println() or "console" or "logger" interface or smth.
+     * @throws IllegalArgumentException
      */
     public ExponentialBackoff(float multiplier, int maxDelaySeconds, boolean verboseFlag) {
+        verifyMultiplierValueOrThrow(multiplier);
+        verifyMaxDelaySecondsOrThrow(maxDelaySeconds);
         mWaitTimeFactor = multiplier;
         mMaxDelayMillis = maxDelaySeconds * 1000;
         mVerbose = verboseFlag;
@@ -67,11 +78,34 @@ public class ExponentialBackoff {
          // otherwise 
          long timeSinceLastAttempt  = millis() - mLastAttemptTime;
          
-         if ( timeSinceLastAttempt >= mCurrentWaitTimeMillis ){ // passed time
+         if ( timeSinceLastAttempt >= mCurrentDelayMillis ){ // passed time
              return true;
          }
          
          return false; // time  still hasn't passed.
+    }
+    
+    
+    /**
+     * This is method for testing/debugging; returns current wait time. 
+     * It's visibility is package, so that only test package can see it.
+     * @return milliseconds.
+     */
+    long getCurrentDelayMillis(){
+        return mCurrentDelayMillis;
+    }
+    
+    long timeSinceLastAttempt(){
+        return millis() - mLastAttemptTime;
+    }
+    
+    /**
+     * Returns value if there is, or 0 if no time to wait.
+     * @return 
+     */
+    long timeLeftToWait(){
+        long leftToWait = getCurrentDelayMillis() - timeSinceLastAttempt();
+        return leftToWait > 0 ? leftToWait : 0;
     }
     
     
@@ -91,7 +125,7 @@ public class ExponentialBackoff {
      * ? how ? first to minimum delay???
      */
     private void resetWaitTime(){
-        mCurrentWaitTimeMillis = C_MIN_WAIT_TIME_MILLIS;
+        mCurrentDelayMillis = C_MIN_WAIT_TIME_MILLIS;
     }
     
     /**
@@ -111,11 +145,37 @@ public class ExponentialBackoff {
 
     
     private void increaseWaitTime() {
-        long newExpDelay = (long) ( mCurrentWaitTimeMillis * mWaitTimeFactor);
+        long newExpDelay = (long) ( mCurrentDelayMillis * mWaitTimeFactor);
         if ( newExpDelay > mMaxDelayMillis ){
             newExpDelay = mMaxDelayMillis;
         }
-        mCurrentWaitTimeMillis  = newExpDelay;
+        mCurrentDelayMillis  = newExpDelay;
+    }
+
+    /**
+     * Verifies that multiplier complies with the spec or throws.
+     * See comment in the constructor {@link ExponentialBackoff}
+     * @param multiplier
+     * @throws IllegalArgumentException 
+     */
+    private void verifyMultiplierValueOrThrow(float multiplier) throws IllegalArgumentException
+    {
+        if ( multiplier < 1.0f ){
+            throw new IllegalArgumentException("Multiplier cannot be less than 1.0, supplied(" + multiplier + ")");
+        }
+    }
+
+    /**
+     * Verifies that max delay parameter fits into spec or throws.
+     * See #ExponetialBackoff constructor comment to know the spec.
+     * @param maxDelaySeconds
+     * @throws IllegalArgumentException 
+     */
+    private void verifyMaxDelaySecondsOrThrow(int maxDelaySeconds) throws IllegalArgumentException
+    {
+         if ( maxDelaySeconds < C_MIN_WAIT_TIME_SECONDS ){
+             throw new IllegalArgumentException("Max delay(" + maxDelaySeconds + ") cannot be less than C_MIN_WAIT_TIME_SECONDS(" + C_MIN_WAIT_TIME_SECONDS + ")");
+         }
     }
     
 }
