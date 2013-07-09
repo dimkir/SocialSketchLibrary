6e8ac23825713ddd25bd714f51f3f1ab026c3114
Dimitry K Lenovo <dimkir@gmail.com>
2013-07-09 12:53:38 +0100

    delegated returning twitter credentials to TwitterCredentials class. Tidied importa

--- test/org/sketchshot/thread/FakeIThreadParams.java
+++ test/org/sketchshot/thread/FakeIThreadParams.java
@@ -1,13 +1,9 @@
 package org.sketchshot.thread;
 
 import org.sketchshot.utils.IConfigXmlSpecification;
-import java.util.HashMap;
 import java.util.Map;
 import org.sketchshot.SysoutLogger;
-import static org.sketchshot.utils.IConfigXmlSpecification.C_CONSUMER_KEY;
-import static org.sketchshot.utils.IConfigXmlSpecification.C_CONSUMER_SECRET;
-import static org.sketchshot.utils.IConfigXmlSpecification.C_OAUTH_SECRET;
-import static org.sketchshot.utils.IConfigXmlSpecification.C_OAUTH_TOKEN;
+import org.sketchshot.TwitterCredentials;
 import org.sketchshot.utils.ILogging;
 
 /**
@@ -81,23 +77,11 @@ implements
 
     
     private Map<String, String> fetchINVALIDCredentials(){
-        Map<String, String>  cred = new HashMap<String, String>();
-        cred.put(C_CONSUMER_KEY, "VctBzerp3P3Wg3oFFBA8NA");
-        cred.put(C_CONSUMER_SECRET, "d4oliVwCrwiO5QO8p95kSygZ6Q4C6pQNXJn1IAyag");
-        cred.put(C_OAUTH_TOKEN, "1413163736-swd4b1RCjMJZLQOmmOJ4zkWKixwcYIAG3LinfVs");
-        cred.put(C_OAUTH_SECRET, "eG5gnsMj3UTxu9Q96GvzWaetgG4mOFlHYBJCVGCmPg");
-        return cred;        
+            return TwitterCredentials.getValidCredentials();
     }
     
     private Map<String, String> fetchValidCredentials(){
-        Map<String, String>  cred = new HashMap<String, String>();
-        cred.put(C_CONSUMER_KEY, "VctBzerp3P3Wg3oFFBA8NA");
-        cred.put(C_CONSUMER_SECRET, "d4oliVwCrwiO5QO8p95kSygZ6Q4C6pQNXJn1IAyag");
-//        cred.put(C_OAUTH_TOKEN, "1413163736-swd4b1RCjMJZLQOmmOJ4zkWKixwcYIAG3LinfVs");
-        cred.put(C_OAUTH_TOKEN, "INVALID");
-//        cred.put(C_OAUTH_SECRET, "eG5gnsMj3UTxu9Q96GvzWaetgG4mOFlHYBJCVGCmPg");
-        cred.put(C_OAUTH_SECRET, "INVALID");
-        return cred;        
+            return TwitterCredentials.getINVALIDCredentials();
     }
     
     /**
